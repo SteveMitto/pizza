48574d40ec269bd4069f20662783e77073e993d5
Steve <mittosteve@gmail.com>
2019-07-27 18:20:17 +0300

    This commit manages the apearing of the order forms

--- js/script.js
+++ js/script.js
@@ -15,4 +15,28 @@ var pizzaTypes = ["Peperoni", "Chicken Tika", "Hawian"]
 $(document).ready(function() {
   console.log("i am ready")
 
+  $(".pizza-types1,.pizza-types2,.pizza-types3").hide();
+
+  $("#pizza1-btn").click(function() {
+    $(".pizza-types1").toggle("slow");
+    $('html, body').animate({
+      scrollTop: $(".pizza-types1").offset().top
+    }, 2000);
+
+  })
+
+  $("#pizza2-btn").click(function() {
+    $(".pizza-types2").toggle("slow");
+    $('html, body').animate({
+      scrollTop: $(".pizza-types2").offset().top
+    }, 1000);
+  })
+
+  $("#pizza3-btn").click(function() {
+    $(".pizza-types3").toggle("slow");
+    $('html, body').animate({
+      scrollTop: $(".pizza-types3").offset().top
+    }, 1000);
+  })
+
 })
